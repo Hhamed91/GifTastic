d5d2ae8953479c6e61a40d7d603d2b6ad3710fc7
user.name <hazem.hamed.h@hotmail.com>
2019-07-28 15:46:22 -0700

    Added last-child to each divimg since it was creating 10 images for each div par3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,22 @@
 $(document).on("click", "button", function () {
     $("#gif-view").empty();
     var query = $(this).val();
-    // console.log($(this).val());
-
-    var gifURL = "https://api.giphy.com/v1/gifs/search?api_key=vMiQJ1wPEvgD6ZYqvMveKHAWZhCCUzXx&q=" + query + "&limit=10&offset=0&rating=G&lang=en"
+    var gifURL = "https://api.giphy.com/v1/gifs/search?api_key=vMiQJ1wPEvgD6ZYqvMveKHAWZhCCUzXx&q="+query+"&limit=10&offset=0&rating=G&lang=en"
 
 
     $.ajax({
         url: gifURL,
         method: "GET"
     }).then(function (response) {
-        for (var i = 0; i < 2; i++) {
+        for (var i = 0; i < 11; i++) {
             $("#gif-view").append("<div>");
             $("#gif-view").attr("class", "divImg");
-            $("#gif-view div").append("<img>");
-            $(".divImg img").attr("src", response.data[i].images.downsized_still.url);
-            $(".divImg img").attr("data-moving", response.data[i].images.downsized_still.url);
-            $(".divImg img").attr("data-still", response.data[i].images.downsized_still.url);
-            $(".divWithGif").append("<span>");
-            $(".divWithGif span").text("Rating " + response.data[i].rating);
+            $("#gif-view div:last-child").append("<img>");
+            $(".divImg img:last-child").attr("src", response.data[i].images.downsized_still.url);
+            $(".divImg img:last-child").attr("data-moving", response.data[i].images.downsized_still.url);
+            $(".divImg img:last-child").attr("data-still", response.data[i].images.downsized_still.url);
+            $(".divImg:last-child").append("<span>");
+            $(".divImg span:last-child").text("Rating " + response.data[i].rating);
         }
     });
 
