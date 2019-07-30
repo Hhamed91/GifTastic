7e124dcbf6b30843a722b9c8e94b0f2b970848a1
user.name <hazem.hamed.h@hotmail.com>
2019-07-30 16:05:29 -0700

    Added a footer and new css format

--- assets/js/app.js
+++ assets/js/app.js
@@ -86,6 +86,9 @@ $("#sub").on("click", function(event) {
 
 $(document).on("click", ".reset", function(){
     $("#gif-Div").empty();
+    $("#gif-Div").html(`<h3>Power by Giphy API</h3>`);
+    $("#gif-Div").append(`<img class= "power" src="assets/img/giphy.gif" atl="Giphy">`);
+
     event.preventDefault();
 
 });
